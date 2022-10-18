85db221e1dce80c2fa4d51b2eacd3a7d3523df85
lochana-vishwajith <lochanavishwajith302@gmail.com>
2022-10-18 09:53:46 +0530

    all xml created

---
+++ app/src/main/java/com/example/fuel_app/Add_checkout_time.java
@@ -0,0 +1,14 @@
+package com.example.fuel_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_checkout_time extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_checkout_time);
+    }
+}
--- app/src/main/java/com/example/fuel_app/Queue_home.java
+++ app/src/main/java/com/example/fuel_app/Queue_home.java
@@ -2,14 +2,26 @@ package com.example.fuel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Queue_home extends AppCompatActivity {
-
+    Button checkIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue_home);
 
+        checkIn = findViewById(R.id.checkInBtn);
+
+        checkIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Queue_home.this,Add_checkin_time.class);
+                startActivity(intent);
+            }
+        });
     }
 }
