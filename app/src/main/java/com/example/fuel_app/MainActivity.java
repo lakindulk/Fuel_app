83bf07bc80cad62081135a4a976ae68cfb09a0c2
K.Jayasekara <K.Jayasekara@P10126>
2022-10-24 13:17:59 +0530

    Updated UI

--- app/src/main/java/com/example/fuel_app/MainActivity.java
+++ app/src/main/java/com/example/fuel_app/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_splash);
 
         //qHomeBtn = findViewById(R.id.qbtn);
 
