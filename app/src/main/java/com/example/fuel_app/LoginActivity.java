0f4b53899c300ecaf28f6c1ba734998bda726504
K.Jayasekara <K.Jayasekara@P10126>
2022-10-24 15:29:17 +0530

    Linked all UIs

--- app/src/main/java/com/example/fuel_app/LoginActivity.java
+++ app/src/main/java/com/example/fuel_app/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.fuel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView signinbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signinbtn = findViewById(R.id.signup);
+
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fuel_app/RegisterActivity.java
+++ app/src/main/java/com/example/fuel_app/RegisterActivity.java
@@ -1,14 +1,31 @@
 package com.example.fuel_app;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView signinbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signinbtn = findViewById(R.id.alreadysignin);
+
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
