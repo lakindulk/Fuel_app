06649a86e7c432abe3cee184f7c3a1fa8c770a22
lakindulk <lakindulk9@gmail.com>
2022-10-25 22:08:56 +0530

    Fuel Type Interfaces Added

---
+++ app/src/main/java/com/example/fuel_app/FueltypeMainActivity.java
@@ -0,0 +1,52 @@
+package com.example.fuel_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FueltypeMainActivity extends AppCompatActivity {
+    Button viewall,viewcount,addstock,addcount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fueltype_main);
+
+        viewall = findViewById(R.id.fueltype_viewall);
+        viewcount = findViewById(R.id.fueltype_view);
+        addstock = findViewById(R.id.fueltype_addfuel);
+        addcount = findViewById(R.id.daily_count);
+
+        viewall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FueltypeMainActivity.this, ViewallActivity.class));
+            }
+        });
+
+        viewcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FueltypeMainActivity.this, ViewcountActivity.class));
+            }
+        });
+
+        addstock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FueltypeMainActivity.this, AddstockActivity.class));
+            }
+        });
+
+        addcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FueltypeMainActivity.this, AdddailyvehiclecountActivity.class));
+            }
+        });
+    }
+
+
+}
