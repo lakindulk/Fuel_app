6b17521686560bff6369c4686ee7ac80ad8be9e2
K.Jayasekara <K.Jayasekara@P10126>
2022-10-26 17:34:43 +0530

    connect buttons

--- app/src/main/java/com/example/fuel_app/OwnerRegistrationActivity.java
+++ app/src/main/java/com/example/fuel_app/OwnerRegistrationActivity.java
@@ -5,23 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class OwnerRegistrationActivity extends AppCompatActivity {
 
-    TextView signinbtn;
+    Button ownerregi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_registration);
 
-        signinbtn = findViewById(R.id.alreadysigninowner);
+        ownerregi = findViewById(R.id.ownerregibtn);
 
-        signinbtn.setOnClickListener(new View.OnClickListener() {
+        ownerregi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(OwnerRegistrationActivity.this,FueltypeMainActivity.class);
+                Intent intent = new Intent(OwnerRegistrationActivity.this,OwnerLoginActivity.class);
                 startActivity(intent);
             }
         });
